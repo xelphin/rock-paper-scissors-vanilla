5cb4d2e23cab0cdd1859a926f0e9bdcf676b5069
xelphin <111276630+xelphin@users.noreply.github.com>
2023-07-13 10:40:12 +0300

    Implement console game

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 console.log("---JS---");
 
-const choice = {
-    0: "rock",
-    1: "paper",
-    2: "scissors"
+const options = {
+    "rock": 0,
+    "paper": 1,
+    "scissors": 2
 }
 
 const stronger = {
@@ -12,26 +12,54 @@ const stronger = {
     "scissors": "paper"
 }
 
+let score = {
+    player: 0,
+    computer: 0
+}
+
+function getPlayerChoice() {
+    let playerChoice = prompt("Please enter your pick");
+    while (options[playerChoice] === undefined) {
+        playerChoice = prompt("Please enter a valid pick");
+    }
+    return playerChoice;
+}
+
 function getComputerChoice() {
     let randNum = Math.floor(Math.random() * 3);
-    return choice[randNum];
+    return Object.keys(options)[randNum];
 }
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase().trim();
-    let res;
-    res = (stronger[playerSelection] === computerSelection) 
-        ? "You Win! "+ playerSelection +" beats "+ computerSelection 
-        : "You Lose! "+ computerSelection +" beats "+ playerSelection ;
+    let res
     if (playerSelection === computerSelection) {
         res = "Tie";
+    }
+    else if (stronger[playerSelection] === computerSelection) {
+        res = "You Win! "+ playerSelection +" beats "+ computerSelection;
+        score.player++;
+    } else {
+        res ="You Lose! "+ computerSelection +" beats "+ playerSelection ;
+        score.computer++;
     }
     return res;
 }
 
-let playerChoice = "rock";
-let computerChoice = getComputerChoice();
+function game() {
+    for(let i=0; i< 5; i++) {
+        console.log(playRound(getPlayerChoice(), getComputerChoice()));
+    }
+    console.log("Player Score: " + score.player);
+    console.log("Computer Score: " + score.computer);
 
-console.log("Player choice: " + playerChoice);
-console.log("Computer choice: " + computerChoice);
-console.log(playRound(playerChoice, computerChoice));
+    if (score.player > score.computer) {
+        console.log("Player is the winner!");
+    } else if (score.player < score.computer) {
+        console.log("Computer is the winner!");
+    } else {
+        console.log("Tie between Player and Computer.");
+    }
+}
+
+game();
